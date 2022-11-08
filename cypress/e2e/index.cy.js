de7f3e0d38f15676d64b8ab2f1d889422b0e8d54
Wanjing Chen <wanjing.chen@thoughtworks.com>
2022-11-08 16:50:39 +0000

    Added test that checks the tags are added and display properly

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -12,17 +12,19 @@ describe("The Home Page UIs", () => {
   });
 
   it("should contains the description", () => {
-    cy.get(".description").contains("Please enter tags below to generate project names.");
+    cy.get(".description").contains(
+      "Please enter tags below to generate project names."
+    );
   });
 
   it("should have input box for adding tags", () => {
-    cy.get('input').should('have.id', 'addTag');
-    cy.get('#addTag').type('text');
+    cy.get("input").should("have.id", "addTag");
+    cy.get("#addTag").should("have.a.property", "text");
   });
 
   it("should have a button for submit tags", () => {
-    cy.get('#Add').type('submit');
-    cy.get('#Add').should('have.value', "Add");
+    cy.get("#Add").type("submit");
+    cy.get("#Add").should("have.value", "Add");
   });
 
   it("should contains the Tags section title", () => {
@@ -30,18 +32,31 @@ describe("The Home Page UIs", () => {
   });
 
   it("should have display box for all the tags", () => {
-    cy.get('.tags').should('be.visible');
+    cy.get(".tags").should("be.visible");
   });
 
   it("should have a submit button for calling the API", () => {
-    cy.get('#Generate').should('have.value', "Generate");
+    cy.get("#Generate").should("have.value", "Generate");
   });
 
   it("should have a window for displaying the results of generated names", () => {
-    cy.get('.results').should('be.visible');
+    cy.get(".results").should("be.visible");
   });
 
   it("should contains the Results section title", () => {
     cy.get("p").contains("Results");
+  });
+});
+
+describe("The Home Page doing work", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("should be able to add new tags", () => {
+    cy.get("#addTag").type("tea");
+    cy.get("#Add").click();
+
+    cy.get(".tags").should('have.text', "tea ");
   });
 });
